276b8bc882f5026470642c0af78ff6b315d90c74
jongmin <90667780+jjong0923@users.noreply.github.com>
2025-08-04 16:52:57 +0900

    Add files via upload

---
+++ Framework/Vue/login01/LoginPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <InputFiled
+      :type="'text'"
+      :placeholder="'Email'"
+      :modelValue="email"
+      v-model="email"
+    />
+    <InputFiled
+      :type="'password'"
+      :placeholder="'Password'"
+      :modelValue="password"
+      v-model="password"
+    />
+    <button @click="login">로그인하기</button>
+    <div>
+      계정이 없으신가요?
+      <span>가입하기</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import InputFiled from "@/components/InputFiled.vue";
+
+export default {
+  name: "LoginPage",
+  components: {
+    InputFiled,
+  },
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      alert(`${this.email} ${this.password}`);
+    },
+  },
+};
+</script>
+
+<style></style>
